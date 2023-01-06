4d12adbafaa412bb1d947a668772408fecfd8e3d
KSHITIJKOLI11 <kshitijkoli11@gmail.com>
2023-01-06 13:45:42 +0530

    day3 task1

---
+++ retail.java
@@ -0,0 +1,9 @@
+public interface retail {
+
+    public double getTotalcost( int tax , int cost , int number );
+
+    public double getTotalextendedcost(double cc, double cp, double ccl);
+
+    public void totaldiscount(double totalcost);
+    
+}
---
+++ shopCompany.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class shopCompany implements retail{
+
+    @Override
+    public double getTotalcost(int tax, int cost, int number) {
+        // TODO Auto-generated method stub
+        double totalcost = (cost*(1 + (tax/100)))*number;
+        return totalcost;
+    }
+
+    @Override
+    public double getTotalextendedcost(double cosmetics, double perfume, double clothes) {
+        // TODO Auto-generated method stub
+        double totalextendedcost = cosmetics + perfume + clothes;
+        System.out.println("total cost: " + totalextendedcost);
+        return totalextendedcost;
+    }
+
+    @Override
+    public void totaldiscount(double totalcost) {
+        // TODO Auto-generated method stub
+        double totaldiscount = (totalcost*(0.98));
+        System.out.println(" Total Discounted price" + totaldiscount);
+        
+    }
+
+    public static void main(String args[]){
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println(" Cosmatics Number: ");
+            int cosmatics = sc.nextInt();
+            System.out.println("Perfume number: ");
+            int perfume = sc.nextInt();
+            System.out.println("clothes number: ");
+            int clothes = sc.nextInt();
+
+            shopCompany r = new shopCompany();
+            double cc = r.getTotalcost(7,1000, cosmatics);
+            System.out.println("Cosmetics total cost: " + cc);
+            double cp = r.getTotalcost(12,5000, perfume);
+            System.out.println("Perfume total cost: " + cp);
+            double ccl = r.getTotalcost(4,7000, clothes);
+            System.out.println("Clothes total cost: " + ccl);
+
+            double totalcost = r.getTotalextendedcost(cc,cp,ccl);
+            r.totaldiscount(totalcost);
+        }
+
+
+
+    }
+
+    
+}
